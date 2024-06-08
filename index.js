f83e7a6565ee1e8f191bc497d0de289ffeed764c
vanshitaprep <81114486+vanshitaroop@users.noreply.github.com>
2024-06-08 21:37:12 +0530

    showing the number of visits

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const redis = require('redis');
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+  host:'redis-server',
+  port:'6379'
+});
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
@@ -12,6 +15,6 @@ app.get('/', (req, res) => {
   });
 });
 
-app.listen(8081, () => {
-  console.log('Listening on port 8081');
-});
+  app.listen(8081, () => {
+    console.log('Listening on port 8081');
+  });
